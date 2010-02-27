ee090378f9a0b874a6aa1f845774650b4d64db1c
Claus Lensbøl <cmol@cmol.dk>
2010-02-27 15:15:49 +0100

    Fixed error with NULL in database

--- app/models/train_lokomotive.rb
+++ app/models/train_lokomotive.rb
@@ -1,8 +1,10 @@
 class TrainLokomotive < ActiveRecord::Base
+ 
+validates_presence_of :operator, :make, :car_type, :user, :decoder
   attr_accessible :operator_id, :lokomotive_class_name, :lokomotive_class_number, :make_id, :lokomotive_type_id, :decoder_adderss, :decoder_id, :box_number, :user_id, :price
-  belongs_to :operator
-  belongs_to :make
-  belongs_to :lokomotive_type
-  belongs_to :decoder
-  belongs_to :user
+belongs_to :operator
+belongs_to :make
+belongs_to :lokomotive_type
+belongs_to :decoder
+belongs_to :user
 end
