e0b7796ea8489852942eb6ffb5967aef5148738d
Claus Lensbøl <cmol@cmol.dk>
2010-02-28 19:33:39 +0100

    Added 5 newest lokomotives and cars to dashboard (and css for that)

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   def index
+  	@lokomotives = TrainLokomotive.all :order => "id DESC", :limit => "5"
+  	@cars = TrainCar.all :order => "id DESC", :limit => "5"
   end
 
 end
