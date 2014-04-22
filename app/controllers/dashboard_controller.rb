e91ff786f87e9ce7f2b4c565fcb3748b546b6174
Claus Lensbøl <cmol@cmol.dk>
2014-04-22 20:34:50 +0200

    Added data to the dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
   def index
+    @locomotives = TrainLocomotive.all :order => "id DESC", :limit => "5"
+    @cars = TrainCar.all :order => "id DESC", :limit => "5"
   end
 end
